73f3fdc61df5343a6ea2ced0d1b57abde1e95d75
jonasfunke <jonasfunke@gmail.com>
2019-03-12 15:59:52 +0100

    analyze_fldingscreen added dot fix

--- analyze_folding_screen.m
+++ analyze_folding_screen.m
@@ -1,9 +1,14 @@
-%% execute this to select 
+%% execute this to select a folder and analyze a folding screen
+
 close all, clear all, clc
 root_path = '/Users/jonasfunke/Dropbox (DIETZ LAB)/FOLDINGSCREENS';
 
 [path_selected] = uigetdir(root_path, 'Select an Directory with intial foling screen');
-[~,dir_name,] = fileparts(path_selected);
+[~,dir_name,ext] = fileparts(path_selected);
+
+if ~isempty(ext)
+    dir_name = [dir_name ext];
+end
 
 txt_files = dir([path_selected filesep '*.txt']);
 tif_files = dir([path_selected filesep '*.tif']);
@@ -13,3 +18,5 @@ if length(tif_files)==1 && length(txt_files)==1
 else
     disp(['Error. Found ' num2str(length(tif_files)) ' tif files and ' num2str(length(txt_files)) ' txt files in ' dir_name])
 end
+
+%%

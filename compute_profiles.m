cfc426952e5f295f5f61d546911cddc50152aa47
elija-feigl <elija.feigl@gmail.com>
2020-11-16 09:49:42 +0100

    feature: (better peak detection) * change from imrect to ROI objects. ROIrectangle for pocket. ROIpolyline for monomer band, ROIpoints for scaffold
    feature (normalization)
    * oligo band marked with ROIpolyline
    * straight line shortcut for  oligo and monomer
    * ladder (if available) marked by ROIpoint
    * changed data layout from cfit to coeff-vector
    * added stapleline and has_ladder to profileData
    
    refactor:
    * remove legacy code
    * update documentation
    * minor style changes

--- compute_profiles.m
+++ compute_profiles.m
@@ -1,37 +1,27 @@
-function [gelData, gelInfo, profileData] = compute_profiles(root_path,dir_name, gel_info_name, image_name)
-%  calculate profiles from gels
-%   Detailed explanation goes here
+function [gelData, gelInfo, profileData] = compute_profiles(root_path, dir_name, gel_info_name, image_name)
+% @ step1
+% calculate profiles from gels
 
-
-    % create output dir
-    prefix_out = dir_name;
     path_out = [root_path filesep dir_name ];
+    file_out = [path_out filesep dir_name];
 
     % load gel_info and check for errors
-    [gelInfo, warnings] = parse_gel_info([root_path filesep dir_name filesep gel_info_name], [root_path filesep dir_name filesep gel_info_name(1:end-4) '_log.log']);
+    [gelInfo, ~] = parse_gel_info([path_out filesep gel_info_name], [path_out filesep gel_info_name(1:end-4) '_log.log']);
     check_parsed_gel_info(gelInfo);
 
-    %% load gel data
-    gelData_raw = load_gel_image('data_dir', [root_path filesep dir_name], 'n_images', 1,...
-        'paths_to_images', {[root_path filesep dir_name filesep image_name]});
-
-    % check for saturation
+    %% via MATLAB_TOOLBOX
+    % load gelData
+    gelData_raw = load_gel_image('data_dir', path_out, 'n_images', 1,...
+        'paths_to_images', {[path_out filesep image_name]});
+    % check and correct raw data
     gelData_raw = check_gel_saturation(gelData_raw);
-
-    %% background correct data
     gelData = background_correct_gel_image(gelData_raw, 'histogram_background', 'on');
-    %gelData.background
-    %gelData = background_correct_gel_image(gelData_raw, 'numberOfAreas', 4);
-    %gelData.images_raw = gelData.images;
-
-
-    %% deterime profiles
-    %profileData = get_gel_lanes(gelData, 'display', 'on', 'cutoff', 0.05, 'selection_type', 'automatic');
+    % get profileData
     profileData = get_gel_lanes(gelData, 'display', 'on', 'cutoff', 0.05, ...
         'number_of_lanes', length(gelInfo.lanes), 'display', 'off', 'move_min_zero', 'Yes');
 
-    %%
-
+    
+    %% check and link data
     if length(profileData.fullProfiles)==length(gelInfo.lanes)
 
         % plot areas 
@@ -44,7 +34,7 @@ function [gelData, gelInfo, profileData] = compute_profiles(root_path,dir_name, 
             text(areas(i,1)+areas(i,3)/2, areas(i,2) , gelInfo.lanes{i}, 'Color', 'r', 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'center', 'FontSize', 8)
         end
         set(gca, 'XTickLabel', [], 'YTickLabel', [])
-        print(cur_fig, '-dpng', '-r 300' , [path_out filesep prefix_out '_lanes.png']); %save figure
+        print(cur_fig, '-dpng', '-r 300' , [file_out '_lanes.png']); %save figure
 
         % plot profiles
     
@@ -62,7 +52,7 @@ function [gelData, gelInfo, profileData] = compute_profiles(root_path,dir_name, 
         %legend(myleg)
         ylabel('Raw Intensity')
         xlabel('Migration distance [px]')
-        print(cur_fig, '-dpdf', [path_out filesep prefix_out '_profiles.pdf']); %save figure
+        print(cur_fig, '-dpdf', [file_out '_profiles.pdf']); %save figure
 
     else
         disp('Warning: number of lanes in gel_info.txt does noth match number of detected lanes.' )
@@ -70,8 +60,8 @@ function [gelData, gelInfo, profileData] = compute_profiles(root_path,dir_name, 
 
     %% save data
     disp('Saving data... please wait')
-    save([path_out filesep prefix_out  '_data.mat'], 'gelData', 'gelInfo', 'profileData' )
-    disp(['Data written to: ' path_out filesep prefix_out  '_data.mat'])
+    save([file_out  '_data.mat'], 'gelData', 'gelInfo', 'profileData' )
+    disp(['Data written to: ' file_out  '_data.mat'])
 
 end
 
--- select_species.m
+++ select_species.m
@@ -1,109 +1,157 @@
-function profileData = select_species(profileData,gelData, sigma_plot)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function profileData = select_species(profileData,gelData, plot_sigma)
+% @ step2
+% select context specfic areas in gel and calculate bands with gauss fit
 
-    peaks_ok='No';
-    while strcmp(peaks_ok,'No')
-        % select are for pockets
+    n = length(profileData.profiles);    
+    peaks_ok= false;
+    while ~peaks_ok
+        %% select species
         plot_image_ui(gelData.images{1});
-        title('Select the pockets')
-
-        h = imrect;
-        wait(h);
-        selectedPocketArea = int32(getPosition(h));
-        close all
-        %sum 
-
-        % compute sum profiles and fit it with gaussian
-        %close all
-        %cf = figure();
-        fits = cell(length(profileData.profiles),1);
-        profile_sum = zeros(selectedPocketArea(4)+1,1);
-
-        for i=1:length(profileData.profiles)
-            profile_sum = profile_sum + profileData.fullProfiles{i}(selectedPocketArea(2):selectedPocketArea(2)+selectedPocketArea(4));
+        % select area for pockets
+        title('Select pockets area')
+        rect_pocket = drawrectangle('Label','Pocket','Color',[1 0 0]);
+        selectedPocketArea = int32(rect_pocket.Position);
+        
+        % select line for monomer band
+        title('Select monomer line (double click to place last)')
+        line_mono = drawpolyline('Label','Monomer','Color',[0 0 1]);
+        pos_mono = line_mono.Position;
+        if length(pos_mono) == 2
+            selectedMono_y = linspace(pos_mono(1,2), pos_mono(2,2), 16);
+            selectedMono = [ones(1,16,'uint32')' selectedMono_y'];
+        else
+            selectedMono = int32(pos_mono);
+        end   
+        
+        
+        % select scaffold bands
+        title('Select Scaffold L')
+        point_scaffL = drawpoint('Label','sL','Color',[0 1 0]);
+        selectedScaffold_L = int32(point_scaffL.Position);
+        title('Select Scafffold R')
+        point_scaffR = drawpoint('Label','sR','Color',[0 1 0]);
+        selectedScaffold_R = int32(point_scaffR.Position);
+        
+        has_ladder = strcmp(questdlg('Ladder?','Does it include a ladder?' ,'No','Yes', 'Yes'),'Yes');
+        if has_ladder
+            % select ladder bands
+            title('Select ladder L')
+            point_ladderL = drawpoint('Label','lL','Color',[0 1 0]);
+            selectedLadder_L = int32(point_ladderL.Position);
+            title('Select ladder R')
+            point_ladderR = drawpoint('Label','lR','Color',[0 1 0]);
+            selectedLadder_R = int32(point_ladderR.Position);
         end
-    
-        %y = mean(gelData.images{1}( selectedPocketArea(2):selectedPocketArea(2)+selectedPocketArea(4), selectedPocketArea(1):selectedPocketArea(1)+selectedPocketArea(3)),2);
-        x = double(selectedPocketArea(2):selectedPocketArea(2)+selectedPocketArea(4));
-        pocket_fit = fit(x', profile_sum, 'gauss1');
-       %%
         
-        %%
+        % select line for staple band
+        title('Select staple line (double click to place last)')
+        line_staple = drawpolyline('Label','Staple','Color',[0.5 0.5 0.5]);
+        pos_staple = line_staple.Position;
+        if length(pos_staple) == 2
+            selectedStaple_y = linspace(pos_staple(1,2), pos_staple(2,2), 16);
+            selectedStaple = [ones(1,16,'uint32')' selectedStaple_y'];
+        else
+            selectedStaple = int32(pos_staple);
+        end    
         
         
-        %clf
-        %plot(x, profile_sum), hold on
-        %plot(x, pocket_fit(x))
-        % integrate pockets
-
-        %% select area for monomer band
-        plot_image_ui(gelData.images{1});
-        title('Select monomer bands')
-        h = imrect;
-        wait(h);
-        selectedArea = int32(getPosition(h));
+        wait(rect_pocket);
+        title('double click pocket-area to finish)')
         close all
+        
+        %% get band data
+        % compute pocket sum profiles and fit it with gaussian.
+        % position and width of pocket is always the same -> sum
+  
+        xpos = selectedPocketArea(2);
+        height = selectedPocketArea(4);
+        y = zeros(height+1,1);
+        for i=1:n
+            y = y + profileData.fullProfiles{i}(xpos:xpos+height);
+        end
+            
+        x = double(xpos:xpos+height);
+        pocket_fit =  coeffvalues(fit(x', y, 'gauss1'));
 
-        %%
-        %cf = figure();
-        fits = cell(length(profileData.profiles),1);
-        tmp = zeros(2, length(profileData.profiles));
-        for i=1:length(profileData.profiles)
-            %figure
 
-            tmp(i,:) = max(profileData.fullProfiles{i}(selectedArea(2):selectedArea(2)+selectedArea(4)));
-
-            y = profileData.fullProfiles{i}(selectedArea(2):selectedArea(2)+selectedArea(4));
-            x = double(selectedArea(2):selectedArea(2)+selectedArea(4));
-
-            fits{i} = fit(x', y, 'gauss1', 'Lower', [0 selectedArea(2) 0], 'Upper', [Inf selectedArea(2)+selectedArea(4) +selectedArea(4)]);
-            %clf
-            %plot(x, y), hold on
-            %plot(x, fits{i}(x))
-
-        end  
+        % compute monomer profiles and fit it with gaussian
+        ladderHeight = 50;
+        scaffoldHeight = 50;
+        bandHeight = 50;
+        mono_fits = zeros(length(profileData.profiles),3);
+        %staple_fits = cell(length(profileData.profiles),1);
+        for i=1:n     
+            if has_ladder
+                if i == 1
+                    height = ladderHeight;
+                    xpos = selectedLadder_L(2) - fix(height/2);
+                elseif i == 2
+                    height = scaffoldHeight;
+                    xpos = selectedScaffold_L(2) - fix(height/2);
+                elseif i == (n-1)
+                    height = scaffoldHeight;
+                    xpos = selectedScaffold_R(2) - fix(height/2);
+                elseif i == n
+                    height = ladderHeight;
+                    xpos = selectedLadder_R(2) - fix(height/2);
+                else
+                    height = bandHeight;
+                    xpos =  selectedMono(i-2,2) - fix(height/2);
+                end
+            else
+                if i == 1
+                    height = scaffoldHeight;
+                    xpos = selectedScaffold_L(2) - fix(height/2);
+                elseif i == n
+                    height = scaffoldHeight;
+                    xpos = selectedScaffold_R(2) - fix(height/2);
+                else
+                    height = bandHeight;
+                    xpos =  selectedMono(i-1,2) - fix(height/2);
+                end
+            end
+            y = profileData.fullProfiles{i}(xpos:xpos+height);
+            x = double(xpos:xpos+height);
+            % TODO: why 2*
+            mono_fits(i,:) = coeffvalues(fit(x', y, 'gauss1', 'Lower', [0 xpos 0], 'Upper', [Inf xpos+ 2 * height])); 
+         
+        end    
 
         
-        
-        %% Display results and aks if ok
+        %% Display results and ask if ok
         close all
-        cur_fig = figure('units','normalized','outerposition',[0 0 1 1]);
+        figure('units','normalized','outerposition',[0 0 1 1]);
         imagesc(gelData.images{1}, [0 3.*std(gelData.images{1}(:))]), axis image, colormap gray, hold on
-        % plot pocket fits
-        for i=1:length(profileData.profiles)
-            plot(mean(profileData.lanePositions(i,1:2)), pocket_fit.b1, 'r.')
-            plot(mean(profileData.lanePositions(i,1:2))*[1 1], [pocket_fit.b1-sigma_plot*pocket_fit.c1 pocket_fit.b1+sigma_plot*pocket_fit.c1], 'r')
+        % plot pocket fits & staple line
+        mu = pocket_fit(2);
+        sig = pocket_fit(3) * plot_sigma;
+        for i=1:n
+            x = profileData.lanePositions(i,1:2);
+            plot(mean(x) , mu, 'r.');
+            plot([mean(x) mean(x)], [mu-sig mu+sig], 'r');
         end
-
         % plot leading band fits
-        for i=1:length(profileData.profiles)
-            plot([profileData.lanePositions(i,1) profileData.lanePositions(i,2) ] , [fits{i}.b1 fits{i}.b1], 'r')
-            plot(mean(profileData.lanePositions(i,1:2))*[1 1], [fits{i}.b1-sigma_plot*fits{i}.c1 fits{i}.b1+sigma_plot*fits{i}.c1], 'r')
+        for i=1:n
+            mu = mono_fits(i,2);
+            sig = mono_fits(i,3) * plot_sigma;
+            x = profileData.lanePositions(i,1:2);
+            plot([x(1) x(2)] , [mu mu], 'r');
+            plot([mean(x) mean(x)], [mu-sig mu+sig], 'r');
         end
-        title(['Band positions with sigma=' num2str(sigma_plot)])
-    
-        
-%         figure(2)
-%         for i=1:length(profileData.profiles)
-%             subplot(ceil(length(profileData.profiles)/3), 3, i)
-%             plot(profileData.fullProfiles{i}); hold on
-%             vline(fits{i}.b1-sigma_plot*fits{i}.c1,'k');
-%             vline(fits{i}.b1+sigma_plot*fits{i}.c1,'k');
-%             vline(pocket_fit.b1-sigma_plot*pocket_fit.c1, 'k');
-%             vline(pocket_fit.b1+sigma_plot*pocket_fit.c1, 'k');
-%             legend(['Lane ' num2str(i)])
-%         end
+        title(['Band positions with sigma *' num2str(plot_sigma)])
 
-        peaks_ok = questdlg('Are the found peaks ok?','Peaks found?' ,'No','Yes', 'Yes');
+        peaks_ok = strcmp(questdlg('Are the found peaks ok?','Peaks found?' ,'No','Yes', 'Yes'),'Yes');
         close all
     end
     
     % add to profiles structure
     profileData.aggregateFit = pocket_fit;
     profileData.aggregateSelectedArea = selectedPocketArea;
-    profileData.monomerFits = fits;
-    profileData.monomerSelectedArea = selectedArea;
+    profileData.monomerFits = mono_fits;
+    profileData.monomerSelectedArea = selectedMono;
+    profileData.stapleLine = selectedStaple;
+    profileData.has_ladder = has_ladder;
+    
 
     
 end
